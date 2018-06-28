fb24b2bc624ccb21b452eeec14c82cc844b0f8c1
Nidhi Jain <nidhij1@fb.com>
2018-06-28 11:26:36 -0700

    added everything

--- app/src/main/java/com/example/nidhij1/login_screen/LoginActivity.java
+++ app/src/main/java/com/example/nidhij1/login_screen/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.nidhij1.login_screen;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onSubmit(View v) {
+        Toast.makeText(getApplicationContext(), "Submitted!", Toast.LENGTH_SHORT).show();
     }
 }

d8deec8a0b54f7d05b9628098377f96d1d922a4e
Nidhi Jain <nidhij1@fb.com>
2018-06-28 10:19:28 -0700

    Initial commit

---
+++ app/src/main/java/com/example/nidhij1/login_screen/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.nidhij1.login_screen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
